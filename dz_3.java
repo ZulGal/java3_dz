a80a90bc3587e37212d8ccf624bc87252ce1254d
matveeva-s <matveeva.svetlana@gmail.com>
2023-05-29 22:31:37 +0300

    Добавили ДЗк семинару 3

---
+++ dz_3.java
@@ -0,0 +1,24 @@
+public class dz_3 {
+    private static class Node {
+        Node next;
+        int value;
+    }
+    static Node head;
+
+    public static void revert() {
+        if (head != null) {
+            Node oldhead = head;
+            Node current = head;
+            while (current.next != null) current = current.next;
+            head = current;
+            Node lastcurrent = current;
+            while (lastcurrent != oldhead) {
+                current = oldhead;
+                while (current.next != lastcurrent) current = current.next;
+                lastcurrent.next = current;
+                lastcurrent = current;
+            }
+        }
+    }
+
+}
